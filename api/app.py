0b1e9ecd84ff9a403b26a642b4d2f31fe26b782d
Carlos Agullo <software.development.2@olympicchannel.com>
2021-07-16 00:15:46 +0200

    updating app.py main routes

--- api/app.py
+++ api/app.py
@@ -2,23 +2,17 @@ import sys
 from flask import Flask
 from flask_restful import Api
 from api.resources.telemetry import Telemetry
-# from myapi.resources.bar import Bar
-# from myapi.resources.baz import Baz
-
-# from flask import Flask
-# from flask_restful import Resource, Api
-# from telemetry import tlm_request_send
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Telemetry, '/telemetry/<int:tlm_channel>/', )
-class HelloWorld(Resource):
-    def get(self):
-        tlm_request_send(1, False);
-        return {'hello': 'world'}
+telemetry = Telemetry()
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Telemetry, '/telemetry', resource_class_kwargs={
+    'tlm_channel': 1,
+    'tc_timeout': 100,
+    'is_continuous': False
+})
 
 if __name__ == '__main__':
     app.run(debug=True)
