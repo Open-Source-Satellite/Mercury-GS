c4401f549314c308ccd1cf2fdc9ed3ede9889de5
Dennis Lien <dennis.lien.o@gmail.com>
2021-09-17 13:11:16 +0200

    WIP: add telecommand send endpoint

--- web_server/main.py
+++ web_server/main.py
@@ -10,4 +10,22 @@ def api_all():
         'pong': 'success',
     })
 
+
+@app.route('/api/v1/telecommand/send/request', methods=['POST'])
+def send_telecommand_request():
+    # TODO: need to add data validation here.
+    # TODO: How does middleware flask work? Need to add Auth JWT M2M token validation.
+    body = request.json
+    telecommand_number = body['telecommand_number']
+    telecommand_data = body['telecommand_data']
+    telecommand_data_type = body['telecommand_data_type']
+    is_continuous = body['is_continuous']
+
+    print('TC: {}'.format(telecommand_number))
+    print('Data: {}'.format(telecommand_data))
+    print('DataType: {}'.format(telecommand_data_type))
+    print('Is continuous: {}'.format(is_continuous))
+
+    return 'OK'
+
 app.run()
