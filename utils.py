021a7f0c1d5d549a66a609354598d2704a720a0d
James Bayley <74238675+KISPE-Jamie@users.noreply.github.com>
2021-11-22 16:10:38 +0000

    Adjust epoch_to_sec milliseconds calculation to round instead of truncate

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def epoch_to_sec(unix_time):
 
     # Milliseconds takes 2 Bytes storage. So, to convert it into unsigned short, lets just take all the bits upto 16 bit.
 
-    millisec_through_sec = int((unix_time - unix_time_seconds)*1000)
+    millisec_through_sec = round((unix_time - unix_time_seconds)*1000)
     milliseconds = (millisec_through_sec & 0xffff)
 
     return unix_time_seconds, milliseconds
